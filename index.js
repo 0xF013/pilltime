a2177437ba731e4c67ae2ceaf286fe3ccfc834eb
Mihail Maxacov <mihail.maxacov@ua.glomex.com>
2017-01-20 15:03:25 +0200

    try req

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const passport = require('koa-passport');
 require('dotenv').config();
 
 const dataSource = knex(require('./knexfile')[process.env.NODE_ENV]);
+const userRepository = require('./lib/repositories/user-repository')(dataSource);
+const timerRepository = {
+  getAll: () => new Promise((resolve) => resolve([{ a: 12 }]))
+};
 
 const app = new Koa();
 const router = new Router();
@@ -23,9 +27,20 @@ app.use(convert(session()));
 app.use(passport.initialize());
 app.use(passport.session());
 
-router.get('/', (ctx) => {
-  console.log(ctx.login());
+router.get('/', ctx => {
   ctx.body = { name: 'Johhny' };
+});
+
+router.get('/timers', async (ctx) => {
+  const timers = await timerRepository.getAll(12);
+  ctx.body = timers;
+});
+
+router.post('/timers', async (ctx, next) => {
+  const timer = await timerRepository.create(Object.assign({}, ctx.params, {
+    user_id: ctx.state.user.id
+  }));
+  ctx.body = timer;
 });
 
 
@@ -40,4 +55,4 @@ app
       ctx.status = err.status || 500;
     }
   })
-  .listen(3000);
+  .listen(3001);
